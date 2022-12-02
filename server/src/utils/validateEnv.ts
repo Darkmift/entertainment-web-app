ec69b437691daf48c745573270c7f222b666717f
darkmift <darkmift@gmail.com>
2022-12-02 20:25:14 +0200

    applied env check for morgan format

--- server/src/utils/validateEnv.ts
+++ server/src/utils/validateEnv.ts
@@ -4,6 +4,9 @@ function validateEnv() {
     cleanEnv(process.env, {
         NODE_ENV: str(),
         PORT: port(),
+        LOG_FORMAT: str({
+            choices: ['combined', 'common', 'dev', 'short', 'tiny'],
+        }),
     });
 }
 
