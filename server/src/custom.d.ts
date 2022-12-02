4b4c0d2a64d9560cd95dad408b463677ef0d0f16
darkmift <darkmift@gmail.com>
2022-12-03 01:41:10 +0200

    new types

--- server/src/custom.d.ts
+++ server/src/custom.d.ts
@@ -1,5 +1,14 @@
 declare namespace Express {
     interface Request {
         uuid?: string;
+        body?: {
+            userId?: string;
+            favoriteItem?: { mediaItemId?: string; toggled?: boolean };
+        };
     }
 }
+
+type DBData = {
+    users: User[];
+    media: MediaItem[];
+};
