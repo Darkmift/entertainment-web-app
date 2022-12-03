893213af53466d50f09ee0466126cf23669080bb
darkmift <darkmift@gmail.com>
2022-12-03 19:59:24 +0200

    added more declarations for request type

--- server/src/custom.d.ts
+++ server/src/custom.d.ts
@@ -1,6 +1,10 @@
 declare namespace Express {
     interface Request {
         uuid?: string;
+        user?: {
+            email: decodedToken.email;
+            id: decodedToken.userId;
+        };
         body?: {
             userId?: string;
             favoriteItem?: { mediaItemId?: string; toggled?: boolean };
