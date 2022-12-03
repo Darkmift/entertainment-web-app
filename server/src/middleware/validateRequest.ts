d2c27acc18f629fd7a4092b2d8eecc321e4ee410
darkmift <darkmift@gmail.com>
2022-12-03 19:58:57 +0200

    refactor for type safety

--- server/src/middleware/validateRequest.ts
+++ server/src/middleware/validateRequest.ts
@@ -1,14 +1,35 @@
 import { logger } from '@/utils/logger';
 import { Request, Response, NextFunction } from 'express';
-import Joi, { ObjectSchema, object, string, array } from 'Joi';
-import { User } from '@/models';
-type GenericJoiSchema = { [key: string]: ObjectSchema };
+import Joi from 'Joi';
 
-const schemas: { [key: string]: Joi.ObjectSchema } = {
-    user: object<User>({
-        _id: string(),
-        email: string().email(),
-        password: string(),
-        favorites: array().items(string()),
+interface Ischemas {
+    [key: string]: Joi.ObjectSchema;
+}
+
+const schemas: Ischemas = {
+    authUser: Joi.object({
+        password: Joi.string().required(),
+        email: Joi.string().email().required(),
     }),
 };
+
+type schemaName = 'authUser';
+type payloadType = 'body' | 'query';
+
+export function joiValidateMiddleware(
+    schemaName: schemaName,
+    payloadType: payloadType,
+) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const { error /*, value*/ } = schemas[schemaName].validate(
+            req[payloadType],
+        );
+        if (error) {
+            const { details } = error;
+            const message = details.map(i => i.message).join(', ');
+            logger.error(message);
+            return res.status(400).send({ error: message });
+        }
+        next();
+    };
+}
