3c12e86c48a4c824726d9919f546b77eaf357804
darkmift <darkmift@gmail.com>
2022-12-05 17:57:27 +0200

    deprecated synchronous methods due to bcrypt bug

--- server/src/services/encryption.ts
+++ server/src/services/encryption.ts
@@ -2,14 +2,6 @@ import bcrypt from 'bcryptjs';
 
 const saltRounds = bcrypt.genSaltSync(10);
 
-export function hash(password: string) {
-    return bcrypt.hashSync(password, saltRounds);
-}
-
-export function compare(password: string, hash: string): boolean {
-    return bcrypt.compareSync(password, hash);
-}
-
 export function hashAsync(password: string) {
     return new Promise((resolve, reject) => {
         bcrypt.hash(password, saltRounds, function (err, hash) {
@@ -19,7 +11,7 @@ export function hashAsync(password: string) {
     });
 }
 
-export function compareasync(password: string, hash: string): Promise<boolean> {
+export function compareAsync(password: string, hash: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
         bcrypt.compare(password, hash, function (err, comparisonResult) {
             if (err) reject(err);
